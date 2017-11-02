1e22d325eac520ea375ac55955fe03176d6519b4
Jennings Anderson <jennings.anderson@colorado.edu>
2017-11-01 22:11:27 -0600

    here we go

---
+++ docs/evacuation/site.js
@@ -0,0 +1,19 @@
+function getURLVars(urlString) {
+  var vars = {};
+  var parts = urlString.substring(0,urlString.indexOf("#")).replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+    vars[key] = value;
+  });
+  return vars;
+}
+
+function getUserFiles(urlString){
+
+  var urlVars = getURLVars(urlString)
+  console.warn(urlVars)
+
+  var rootP = urlVars['root'] || 'sample/';
+  var uName = urlVars['user'] || '48hippo'
+
+  return [rootP, uName+'.geojson',uName+'-meta.geojson']
+
+}
